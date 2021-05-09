1e7531339b308ad3d84c8abcceb4b57ed51f9ef7
EgorShubin <egor.shubin@dodreams.com>
2021-05-09 13:01:40 +0200

    initial commit. created quarkus project

---
+++ src/main/java/com/example/message/service/message/MessageController.java
@@ -0,0 +1,15 @@
+package com.example.message.service.message;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/message")
+public class MessageController {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello, quarkus";
+    }
+}
---
+++ src/test/java/com/example/message/service/message/NativeMessageControllerIT.java
@@ -0,0 +1,9 @@
+package com.example.message.service.message;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeMessageControllerIT extends MessageControllerTest {
+
+    // Execute the same tests but in native mode.
+}
